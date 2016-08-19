d29ea496de159d8d75fc2c6104156cb0bf773443
garmoncheg <garmon1@gmail.com>
2016-08-19 11:13:25 +0300

    Django polls tutorial part 7

--- polls/models.py
+++ polls/models.py
@@ -9,12 +9,15 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
-
-    def __str__(self):
-        return self.question_text
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
